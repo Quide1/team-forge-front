5bd5a5a9a2883ccb0a912ea62c423efd4cc1f695
Quide1 <ezequielr.dev@gmail.com>
2024-09-03 20:40:34 -0300

    create a navbar

--- app/sections/navbar-landing.tsx
+++ app/sections/navbar-landing.tsx
@@ -2,9 +2,9 @@ import { Users, Rocket, Zap, ArrowRight, Anvil } from "lucide-react";
 import Link from "next/link";
 function NavBarLandingPage() {
   return (
-    <header className="bg-gray-600 flex px-4 lg:px-6 h-14 items-center justify-between">
+    <header className=" flex px-4 lg:px-6 h-14 items-center justify-between">
       <Link className="flex items-center justify-center" href="#">
-        <Anvil className="h-6 w-6 text-red-600" />
+        <Anvil className="h-6 w-6 text-custom-palette-500" />
         <span className="ml-2 text-2xl font-bold ">ForgeTeam</span>
       </Link>
       <nav className="flex flex-row gap-2 text-wrap items-center justify-center py-4 lg:gap-4 text-sm">
