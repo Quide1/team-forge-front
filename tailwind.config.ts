fc5c12d6fac20241a55a76f8357c12bef717a84b
Quide1 <ezequielr.dev@gmail.com>
2024-09-03 20:39:49 -0300

    add new palette colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -13,7 +14,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+      
+        'custom-palette': {
+        '50': '#fffeea',
+        '100': '#fffac5',
+        '200': '#fff685',
+        '300': '#ffea46',
+        '400': '#ffdb1b',
+        '500': '#ffbb00',
+        '600': '#e29000',
+        '700': '#bb6502',
+        '800': '#984e08',
+        '900': '#7c400b',
+        '950': '#482100',
     },
+    
+      
+    }
+    },
+   
   },
   plugins: [],
 };
