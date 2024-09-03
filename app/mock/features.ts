b111934dde3c4494deebc9ff7cd41d9a231c3ba8
Quide1 <ezequielr.dev@gmail.com>
2024-09-03 20:40:27 -0300

    data mock

---
+++ app/mock/features.ts
@@ -0,0 +1,38 @@
+export const featuresContent = [
+    {   
+        id:"329483209",
+        title:"titulo",
+        paraph:" Encontrarás una gran cantidad de personas dispuestas a crear impresionantes proyectos cooperando contigo"
+    },
+    {   
+        id:"329483209",
+        title:"titulo",
+        paraph:"Desde aplicaciones web hasta innovaciones en IA, da vida atus ideas con un equipo diverso y capacitado."
+    },
+    {   
+        id:"329483209",
+        title:"titulo",
+        paraph:" Aprende de otros, expande tu red de contactos y construye unportafolio que destaque en la industria."
+    },
+]
+
+export const howToWorkInfo = [
+    {
+      title: "Regístrate",
+      description: "Crea tu perfil y muestra tus habilidades",
+    },
+    {
+      title: "Encuentra un Equipo",
+      description:
+        "Explora proyectos o encuentra el equipo perfecto",
+    },
+    {
+      title: "Colabora",
+      description:
+        "Trabaja junto a otros utilizando nuestras herramientas integradas",
+    },
+    {
+      title: "Triunfa",
+      description: "Completa tu proyecto y expande tu portafolio",
+    },
+  ]
